1ed003427463f37e8add82e840893ca01319ada2
ewgo <ewelina.gonda@gft.com>
2016-09-08 16:21:47 +0200

    shit

--- app/app.js
+++ app/app.js
@@ -1,7 +1,17 @@
-$(document).ready(function(){
- $("#msgid").html("This is Hello World by JQuery");
- $.fn.myFunction = function(){
-     alert('You have successfully defined the function!'); 
- }
+$(document).ready(function () {
+    //  $("#msgid").html("This is Hello World by JQuery");
+    //  $.fn.myFunction = function(){
+    //      alert('You have successfully defined the function!'); 
+    //  }
+
+
+    $('button').on('click', function () {
+        window.location.replace("http://stackoverflow.com");
+    });
+
+    $.router.add("/signin", function(){
+        alert('You have successfully defined the function!'); 
+    });
+
 
 });
